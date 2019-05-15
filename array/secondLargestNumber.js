6a19bf83bdee1fc3853311d39c35c7ef43705251
Sandip Nirmal <sandipnirmal@icloud.com>
2019-05-15 22:36:02 +0530

    Create secondLargestNumber.js

---
+++ array/secondLargestNumber.js
@@ -0,0 +1,12 @@
+/**
+ * Function which returns second largest number from given array
+ *
+ * @param {array} arr
+ * 
+ * @returns {number}
+ */
+function find2ndLargestNumber(arr){
+
+const filteredArr = arr.filter((item, index) => arr.indexOf(item) === index)
+return filteredArr.sort((a, b) => a - b)[filteredArr.length - 2]
+}
