4f72372976efaab84608a144288ffb455292ceac
Sandip Nirmal <sandipnirmal@icloud.com>
2019-05-16 08:52:56 +0530

    Update secondLargestNumber.js

--- array/secondLargestNumber.js
+++ array/secondLargestNumber.js
@@ -5,10 +5,15 @@
  * 
  * @returns {number}
  */
-function find2ndLargestNumber(arr){
-
+function find2ndLargestNumber(arr) {
   const filteredArr = arr.filter((item, index) => arr.indexOf(item) === index)
-  return filteredArr.sort((a, b) => a - b)[filteredArr.length - 2]
+  return filteredArr.length > 1
+			? filteredArr.sort((a, b) => a -b )[filteredArr.length - 2]
+			: filteredArr[0]
 }
+
+find2ndLargestNumber([1, 2, 3, 4, 5, 4, 5]) // 4
+find2ndLargestNumber([1, 2]) // 1
+find2ndLargestNumber([1, 2, 3, 4, 6, 7, 5, 3, 8, 9, 1, 2, 9, 8, 8, 9]) // 8
 
 // One can use Map as well to filter unique items.
